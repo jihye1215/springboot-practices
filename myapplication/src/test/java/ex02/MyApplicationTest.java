4a0c8987d5e4137a432017682ea38436ef8bd4f4
wlgp1 <wlgp1@DESKTOP-5T5NIVG>
2022-02-10 20:08:19 +0900

    springboot-practices #1

--- myapplication/src/test/java/ex02/MyApplicationTest.java
+++ myapplication/src/test/java/ex02/MyApplicationTest.java
@@ -1,4 +1,4 @@
-package ex01;
+package ex02;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -6,11 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @ExtendWith(SpringExtension.class)
-// @ContextConfiguration(classes= {MyApplication.class})
-/* @Configuration 설정 클래스는 자동으로 찾지 못함 */
+
 @SpringBootTest
-public class MyApplicationTest02 {
+public class MyApplicationTest {
 	@Autowired
 	private MyComponent myComponent;
 	
--- myapplication/src/test/java/ex04/MyApplicationTest.java
+++ myapplication/src/test/java/ex04/MyApplicationTest.java
@@ -1,4 +1,4 @@
-package ex01;
+package ex04;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @ExtendWith(SpringExtension.class)
-// @ContextConfiguration(classes= {MyApplication.class})
-/* @Configuration 설정 클래스는 자동으로 찾지 못함 */
+import ex04.component.MyComponent;
+
+
 @SpringBootTest
-public class MyApplicationTest02 {
+public class MyApplicationTest {
 	@Autowired
 	private MyComponent myComponent;
 	
